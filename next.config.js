7b8c939731380e19c283a71c98f761f8de4914f9
Troy Underwood <troyizzle@gmail.com>
2021-07-02 18:47:01 -0400

    Add production url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "tailwindui.com", "images.unsplash.com"],
+    domains: [
+      "localhost",
+      "tailwindui.com",
+      "images.unsplash.com",
+      "https://gizmo-pets-api.herokuapp.com",
+    ],
   },
 };
